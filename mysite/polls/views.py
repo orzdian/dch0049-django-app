59d475d5b8932adf9765a1fb6ec047e9cb444d5f
orzdian <98924155+orzdian@users.noreply.github.com>
2022-10-24 20:51:46 -0400

    update mysite

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
-from .models import Question
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
+from .models import Question
 
 
 def index(request):
@@ -13,7 +13,6 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -21,3 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
